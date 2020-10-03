2ff479a1daf291fe06cd691ed3633162df7b04ba
Sourav Debnath <sourav2012d@gmail.com>
2020-10-03 13:32:17 +0530

    Adds Stack

---
+++ Stack.js
@@ -0,0 +1,34 @@
+/**
+ * @author Sourav Debnath <sourav2012d@gmail.com>
+ *
+ * @constructor
+ * @param {array} data - The array utilized to implement stack.
+ */
+
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  /**
+   * @function push - Inserts element in the Stack
+   * @param {dataType} - The data to be inserted in the stack
+   */
+  push(item) {
+    this.data.push(item);
+  }
+
+  /**
+   * @function pop - Retrieves element from the Stack Top
+   */
+  pop() {
+    return this.data.pop();
+  }
+
+  /**
+   * @function peek - Retrieves element from the Stack Top without removing it from the Stack
+   */
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
